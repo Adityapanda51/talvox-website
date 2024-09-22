7cd117df6b2679e096723769adc59bca66ddec19
Aditya Kumar Panda <adityapanda15@gmail.com>
2024-09-22 12:59:41 +0530

    whatsapp

---
+++ src/components/WhatsAppChatButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../Styles/WhatsAppChatButton.css'; 
+
+const WhatsAppChatButton = () => {
+  const phoneNumber = '+919959002091'; // Replace with your WhatsApp number in international format
+  const message = 'Hello, How Can I Help You?'; // Your predefined message
+  const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+
+  return (
+    <a href={url} target="_blank" rel="noopener noreferrer" className="whatsapp-button">
+      <i className="fab fa-whatsapp"></i> {/* Font Awesome icon */}
+    </a>
+  );
+};
+
+export default WhatsAppChatButton;
